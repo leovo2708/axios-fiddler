ab28726d3c21fdd3a28a5d4af80a11270e48e1e5
leodl18 <chikichaka@gmail.com>
2019-11-08 01:24:19 +0700

    test post formdata

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ process.env['HTTP_PROXY'] = 'http://127.0.0.1:8888';
 process.env['HTTPS_PROXY'] = 'http://127.0.0.1:8888';
 
 const axios = require('axios').default;
+const lodash = require('lodash');
+const querystring = require('querystring');
 
-axios.get('https://www.google.com').then(function (response) {
+function toQueryString(options) {
+    const params = lodash.extend({}, options, { key: 'B-vN0Z8ZFAqFwwitDpWnqrKTngAZMRp' });
+    return querystring.stringify(params);
+}
+
+const queryString = toQueryString({});
+const id = 'main_window.title';
+const locale = 'ko-KR';
+const data = querystring.stringify({
+    flag: 'incomplete'
+})
+axios.post(`https://localise.biz/api/translations/${id}/${locale}/flag`, data, {
+    headers: {
+        Authorization: `Loco B-vN0Z8ZFAqFwwitDpWnqrKTngAZMRp1X`
+    }
+}).then(function (response) {
     // handle success
     console.log(response);
 }).catch(function (error) {
